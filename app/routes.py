d730e655a8565fdc4cc42a629ad4b945f4cad600
Vacunto <vaggel1993@gmail.com>
2025-05-19 20:01:15 +0300

    #3 Добавил работоспособность приветствия

--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,19 @@
-from flask import Flask, render_template_string
+from flask import Flask, render_template_string, request
 
 app = Flask(__name__)
 
 
-@app.route("/", methods=["GET"])
+@app.route("/", methods=["GET", "POST"])
 def index():
+    name = ""
+    if request.method == "POST":
+        name = request.form.get("str", "")
     return render_template_string("""
         <form method="post">
             <input name="name" placeholder="Введите имя">
             <input type="submit">
         </form>
-    """)
+        {% if name %}
+        <h1>Hello, {{ name }}!</h1>
+        {% endif %}
+    """, name=name)
